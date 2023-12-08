4f6c5d0111adb56f4183d33cc592c920e8fc0838
alifnasrullah <136994603+alifnasrullah@users.noreply.github.com>
2023-12-08 21:45:24 +0700

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connectToDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://admin:T3k4dB4j4@infinitenote.0bai7rf.mongodb.net/infinitenote"
+      "mongodb+srv://19210607:1996-12-02@infinitenote.ulacsob.mongodb.net/infinitenote"
     )
     if (connection) {
       console.log("Connection established")
